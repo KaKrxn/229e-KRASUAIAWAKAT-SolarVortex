81fe4a56f295c745edcf1e2b476e17c576ddeb09
KaKrxn <itthichai.faen@bumail.net>
2025-03-31 16:39:01 +0700

    UpdateCredit Scene

---
+++ Assets/Boss/scirpt/LinkAssetLoader.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class LinkAssetLoader : MonoBehaviour
+{
+    public void OpenWebsite()
+    {
+        Application.OpenURL("https://github.com/KaKrxn/ModelFree.git");
+    }
+}
